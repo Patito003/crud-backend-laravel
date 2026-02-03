acc753c87e5485f400eb32f90399598f4c77995a
Paulo Vitor Martins de Castro <paulomartins31205@gmail.com>
2026-02-03 15:55:50 -0300

    fix: Padronizando rotas

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/sistema-legado', [SistemaLegadoController::class, 'listar']);
-Route::get('/sistema-legado/{id}', [SistemaLegadoController::class, 'buscar']);
+Route::get('/sistemas', [SistemaLegadoController::class, 'listar']);
+Route::get('/sistemas/{id}', [SistemaLegadoController::class, 'buscar']);
 
-Route::post('/sistema-legado', [SistemaLegadoController::class, 'cadastrar']);
+Route::post('/sistemas', [SistemaLegadoController::class, 'cadastrar']);
 
-Route::put('/sistema-legado/{id}', [SistemaLegadoController::class, 'atualizar']);
+Route::put('/sistemas/{id}', [SistemaLegadoController::class, 'atualizar']);
 
-Route::delete('/sistema-legado/{id}', [SistemaLegadoController::class, 'remover']);
+Route::delete('/sistemas/{id}', [SistemaLegadoController::class, 'remover']);
